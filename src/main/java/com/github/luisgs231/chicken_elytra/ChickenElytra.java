2bf042ec759301032ea40efcc4d5fe06bca1b431
Luis <luisgs231@yahoo.es>
2020-05-18 23:05:42 +0200

    E4

--- src/main/java/com/github/luisgs231/chicken_elytra/ChickenElytra.java
+++ src/main/java/com/github/luisgs231/chicken_elytra/ChickenElytra.java
@@ -1,17 +1,49 @@
 package com.github.luisgs231.chicken_elytra;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.minecraftforge.fml.common.Mod;
+import java.util.stream.Collectors;
 
 @Mod("chicken_elytra")
 public class ChickenElytra {
 	public static final String MODID = "chicken_elytra";
+	public static ChickenElytra instance;
 	private static final Logger LOGGER = LogManager.getLogger();
 	
 	public ChickenElytra() {
-		LOGGER.debug("Probando esto, con git también... xD");
-	}
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        
+        instance = this;
+        MinecraftForge.EVENT_BUS.register(this);
+    }
 	
+	private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+	
+	private void doClientStuff(final FMLClientSetupEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+
+    }
 
 }
---
+++ src/main/java/com/github/luisgs231/chicken_elytra/init/BlockInit.java
@@ -0,0 +1,31 @@
+package com.github.luisgs231.chicken_elytra.init;
+
+import com.github.luisgs231.chicken_elytra.ChickenElytra;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.registries.ObjectHolder;
+
+@ObjectHolder(ChickenElytra.MODID)
+@Mod.EventBusSubscriber(modid = ChickenElytra.MODID, bus = Bus.MOD)
+public class BlockInit {
+	public static final Block test_block = null;
+
+	
+	@SubscribeEvent
+	public static void registerBlocks(final RegistryEvent.Register<Block> event) {
+		event.getRegistry().register(new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(0.7f, 14.0f)).setRegistryName(ChickenElytra.MODID, "test_block"));
+	}
+	
+	@SubscribeEvent
+	public static void registerBlockItems(final RegistryEvent.Register<Item> event) {
+		event.getRegistry().register(new BlockItem(test_block, new Item.Properties().maxStackSize(64).group(ItemGroup.MISC)).setRegistryName("test_block"));
+	}
+}
