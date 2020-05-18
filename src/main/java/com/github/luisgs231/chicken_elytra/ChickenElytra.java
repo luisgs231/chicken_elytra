1b56638a2791f02a837e4bb66ee178652d11b2da
Your Name <you@example.com>
2020-05-18 15:05:26 +0200

    init

---
+++ src/main/java/com/github/luisgs231/chicken_elytra/ChickenElytra.java
@@ -0,0 +1,17 @@
+package com.github.luisgs231.chicken_elytra;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("chicken_elytra")
+public class ChickenElytra {
+	public static final String MODID = "chicken_elytra";
+	private static final Logger LOGGER = LogManager.getLogger();
+	
+	public ChickenElytra() {
+		LOGGER.debug("Probando esto... xD");
+	}
+	
+
+}
